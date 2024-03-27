972a9bd53c388f04c180fb9f41b8cd27b1acc27b
Ng Chin Chia <billyngchinchia@gmail.com>
2024-03-27 17:50:51 +0800

    fix: alignment

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import AlertNotification from '@/components/notification/AlertNotification.vue'
 </script>
 
 <template>
-  <div class="flex flex-col space-y-10 items-center justify-center my-80">
+  <div class="flex flex-col space-y-10 items-center justify-center my-20">
     <ReusableButton variant="primary">Action</ReusableButton>
     <ReusableButton variant="primary" :left-icon="ArrowIcon" />
     <ReusableButton variant="primary" :left-icon="ArrowIcon"> Action </ReusableButton>
